53e19abdddcf7aae3215f2d4a884fa5a9336a443
Mulih <almulih@yahoo.com>
2024-05-23 10:26:57 +0300

    set dev: nodemon app.js

--- src/server/models/database.js
+++ src/server/models/database.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
-mongoose.connect(process.env.MONGODB_URI);
+mongoose.connect(process.env.MONGODB_URI, {
+    ssl: true,
+    sslValidate: true
+});
 
 const db = mongoose.connection;
 db.on('error', console.error.bind(console, 'connection error.'));
